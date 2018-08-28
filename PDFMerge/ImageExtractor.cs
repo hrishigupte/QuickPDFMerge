79960a0c8328af1ec1486f6708a6bbb2f6ac917d
Hrishi Gupte <hrishigupte@yahoo.com>
2018-08-28 19:30:12 -0400

    Added code to handle DCTDecode and JBIG2Decode

--- PDFMerge/ImageExtractor.cs
+++ PDFMerge/ImageExtractor.cs
@@ -7,6 +7,9 @@ using iTextSharp.text;
 using System.IO;
 using iTextSharp.text.pdf;
 using System.Drawing;
+using System.Collections;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace PDFMerge
 {
@@ -38,27 +41,114 @@ namespace PDFMerge
                 if (xobj==null) continue;
                 var keys = xobj.Keys;
                 if (keys.Count==0) continue;
-                foreach (var k in keys)
-                    Console.WriteLine(k);
-                PdfName nm = keys.OfType<PdfName>().SingleOrDefault();
-                var obj = xobj.Get(nm);
-                if (!obj.IsIndirect()) continue;
+                //foreach (var k in keys)
+                //    Console.WriteLine(k);
+                string width ="",height="",bitspc="",filter="";
+                foreach (var nm in keys)
+                {
+                //PdfName nm = keys.OfType<PdfName>().SingleOrDefault();
+                    var obj = xobj.Get((PdfName)nm);
+                    if (!obj.IsIndirect()) continue;
+                    var tg = PdfReader.GetPdfObject(obj) as PdfDictionary;
+                    var type = tg.Get(PdfName.SUBTYPE) as PdfName;
+                    
+                    foreach (PdfName t in tg.Keys)
+                    {
+                       // Console.WriteLine(t);
+                       switch (t.ToString().ToUpper())
+                       {
+                            case "/WIDTH":
+                                width = tg.Get(t).ToString();
+                                break;
+                            case "/HEIGHT":
+                                height = tg.Get(t).ToString();
+                                break;
+                            case "/FILTER":
+                                filter = tg.Get(t).ToString();
+                                break;
+                            case "/BITSPERCOMPONENT":
+                                bitspc = tg.Get(t).ToString();
+                                break;
+                            default:
+                                break;
+                       }
 
-                var tg = PdfReader.GetPdfObject(obj) as PdfDictionary;
-                var type = PdfReader.GetPdfObject(tg.Get(PdfName.SUBTYPE)) as PdfName;
-                var filter = PdfReader.GetPdfObject(tg.Get(PdfName.FILTER)).ToString();
-                Console.WriteLine(filter);
-                
-                if (!PdfName.IMAGE.Equals(type)) continue;
-                Console.WriteLine(filter);
-                if (!(filter=="/DCTDecode"||filter=="/FlatDecode"))
-                    continue;
+                    }
+                    
+                    
+                    if (!PdfName.IMAGE.Equals(type)) continue;
+                    //if (!(filter=="/DCTDecode"||filter=="/FlatDecode" ||filter.Contains("Obj")))
+                    //    continue;
 
-                int xrefIndex = (obj as PRIndirectReference).Number;
-                var pdfStream = pdfreader.GetPdfObject(xrefIndex) as PRStream;
-                var data = PdfReader.GetStreamBytesRaw(pdfStream);
-                File.WriteAllBytes(outputFileName,data);
-
+                    int xrefIndex = (obj as PRIndirectReference).Number;
+                    var pdfStream = pdfreader.GetPdfObject(xrefIndex) as PRStream;
+                    var data = PdfReader.GetStreamBytesRaw(pdfStream);
+                    
+                    if (filter=="/DCTDecode")
+                    {
+                        File.WriteAllBytes(outputFileName,data);
+                    }
+                    else if (filter=="/JBIG2Decode")
+                    {
+                      
+                        int wd = int.Parse(width);
+                        int ht = int.Parse(height);
+                        int bpp = int.Parse(bitspc);
+                        PixelFormat format = PixelFormat.Format1bppIndexed;
+                        switch (bpp)
+                        {
+                            case 1: 
+                                format = PixelFormat.Format1bppIndexed;
+                                break;
+                            case 8:
+                                format = PixelFormat.Format8bppIndexed;
+                                break;
+                            case 24:
+                                format = PixelFormat.Format24bppRgb;
+                                break;
+                            default:
+                                break;
+                        }
+                        using (Bitmap bmp = new Bitmap(wd,ht,format))
+                        {
+                            BitmapData bmd = bmp.LockBits(new System.Drawing.Rectangle(0,0,wd,ht),ImageLockMode.WriteOnly,format);
+                            Marshal.Copy(data,0,bmd.Scan0,data.Length);
+                            bmp.UnlockBits(bmd);
+                            using (MemoryStream bmpstream = new MemoryStream())
+                            {
+                                bmp.Save(bmpstream,ImageFormat.Tiff);
+                                File.WriteAllBytes(outputFileName.Replace("jpg","tiff"),bmpstream.GetBuffer());
+                                bmpstream.Close();
+                            }
+                        }
+                      
+                     
+                       //File.WriteAllBytes(outputFileName.Replace("jpg","jbig2"),ldata);
+                       
+                    }
+                    else 
+                    {
+                        var bytedata = PdfReader.FlateDecode(data);
+                        File.WriteAllBytes(outputFileName,bytedata);
+                    }
+                    
+                    /* MemoryStream stream = new MemoryStream(data);
+                    //var image = new MagickImage(stream);
+                    //image.Write(outputFileName);
+                    Bitmap bmp = new Bitmap(stream);
+                    //bmp.Save(outputFileName);
+                    using (MemoryStream mspng = new MemoryStream())
+                    {
+                        bmp.Save(mspng,System.Drawing.Imaging.ImageFormat.Png);
+                        int l = (int)mspng.Length;
+                        byte[] pngdata = new byte[l];
+                        mspng.Read(pngdata,0,l);
+                        File.WriteAllBytes(outputFileName,pngdata);
+                        mspng.Close();
+                        stream.Close();
+                    }
+                    //File.WriteAllBytes(outputFileName,data); */
+                }
             }
             
            // return null;
